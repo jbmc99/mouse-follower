91f5285cc47d0e1b7c6eb676395ae57921ca8f68
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-08 23:40:09 +0200

    upd

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -3,35 +3,39 @@ function App() {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
   useEffect(() => {
-    console.log("effect", { enabled })
+    console.log("effect", { enabled });
     const handleMove = (event) => {
       const { clientX, clientY } = event;
-      console.log('handleMove', { clientX, clientY })
+      console.log("handleMove", { clientX, clientY });
       setPosition({ x: clientX, y: clientY });
-    }
+    };
     if (enabled) {
-      window.addEventListener('pointermove', handleMove)
-    } 
-
-    return () => {
-      window.removeEventListener('pointermove', handleMove)
+      window.addEventListener("pointermove", handleMove);
     }
+    // cleanup
+    // cuando el componente se desmonta
+    // cuando cambian las dependencias, antes de ejecutar el
+    // efecto de nuevo
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
   }, [enabled]);
 
   return (
     <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'red',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: `translate(${position.x}px, ${position.y}px)`,
-      }}
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "red",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
       />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? "Desactivar" : "Activar"} seguir puntero
